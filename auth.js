cecb4963e209444ed9e50030527bb6ad5ef88749
alokolodo <37911297+alokolodo@users.noreply.github.com>
2024-07-25 00:43:00 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,42 +1,37 @@
 const jwtSecret = 'your_jwt_secret';
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
-require('./passport'); // Include local passport file
+require('./passport'); // Your local passport file
 
-const express = require('express');
-const router = express.Router();
+const { Router } = require('express');
+const router = Router();
+
+const generateJWTToken = (user) => {
+  return jwt.sign(user, jwtSecret, {
+    subject: user.username,
+    expiresIn: '7d',
+    algorithm: 'HS256'
+  });
+}
 
 router.post('/login', (req, res) => {
   passport.authenticate('local', { session: false }, (err, user, info) => {
-    if (err) {
-      console.error('Authentication error:', err);
+    if (err || !user) {
       return res.status(400).json({
         message: 'Something is not right',
-        error: err
+        user: user
       });
     }
-
-    if (!user) {
-      console.error('User not found or incorrect password:', info);
-      return res.status(400).json({
-        message: 'Something is not right',
-        info: info
-      });
-    }
-
     req.login(user, { session: false }, (err) => {
       if (err) {
-        console.error('Login error:', err);
         res.send(err);
       }
-
-      const token = jwt.sign(user.toJSON(), jwtSecret, {
-        expiresIn: '1h'
-      });
-
+      const token = generateJWTToken(user.toJSON());
       return res.json({ user, token });
     });
   })(req, res);
 });
 
-module.exports = router;
+module.exports = (app) => {
+  app.use('/', router);
+};
