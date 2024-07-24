64c614eda8c72d6dc25e7f450bcfed90b38492ff
alokolodo <37911297+alokolodo@users.noreply.github.com>
2024-07-25 00:46:06 +0100

    Update models.js

--- models.js
+++ models.js
@@ -4,35 +4,22 @@ const bcrypt = require('bcrypt');
 let movieSchema = mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
-  genre: { type: String, required: true },
+  genre: {
+    name: String,
+    description: String
+  },
   director: {
     name: String,
     bio: String,
-    birth: Date,
-    death: Date
+    birth: Date
   },
-  imageUrl: String,
+  actors: [String],
+  imagePath: String,
   featured: Boolean
 });
 
 let userSchema = mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
-  email: { type: String, required: true },
-  birthdate: Date,
-  favoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+  email: {Certainly! `Server is running on http://localhost:${port}`);
 });
-
-userSchema.statics.hashPassword = function(password) {
-  return bcrypt.hashSync(password, 10);
-};
-
-userSchema.methods.validatePassword = function(password) {
-  return bcrypt.compareSync(password, this.password);
-};
-
-let Movie = mongoose.model('Movie', movieSchema);
-let User = mongoose.model('User', userSchema);
-
-module.exports.Movie = Movie;
-module.exports.User = User;
